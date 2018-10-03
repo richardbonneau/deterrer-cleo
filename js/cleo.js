4700707ca3f3d51dee2bdbab0a6dc9a38da00fba
unknown <richardbonneau@hotmail.com>
2018-10-02 20:24:36 -0400

    some background animation work, start and end of level animations

--- js/cleo.js
+++ js/cleo.js
@@ -1,27 +1,58 @@
-// This section contains some game constants. It is not super interesting
+//  Widths and Heights
 var GAME_WIDTH = 450;
 var GAME_HEIGHT = 500;
 
+var PLAYER_WIDTH = 50;
+var PLAYER_HEIGHT = 100;
+
 var ENEMY_WIDTH = 75;
-var ENEMY_HEIGHT = 156;
-var MAX_ENEMIES = 3;
+var ENEMY_HEIGHT = 75;
+var MAX_ENEMIES = 1;
 
-var PLAYER_WIDTH = 75;
-var PLAYER_HEIGHT = 150;
+var BOUFFE_WIDTH = 75;
+var BOUFFE_HEIGHT = 100;
 
-// These two constants keep us from using "magic numbers" in our code
-var LEFT_ARROW_CODE = 37;
-var RIGHT_ARROW_CODE = 39;
-// These two constants allow us to DRY
-const MOVE_LEFT = 'left';
-const MOVE_RIGHT = 'right';
-//  Smooth Movement
+
+//  Start and Finish
+var BARN_WIDTH = 450;
+var BARN_HEIGHT = 125;
+var PARADISE_WIDTH = 450;
+var PARADISE_HEIGHT = 300;
+
+var barnStart = false;
+var paradiseStart = false;
+
+var didPlayerWinGame = false;
+
+
+//  Levels
+var level0 = true;
+var level1 = true;
+
+
+//  Player Movement
 var playerMoveLeft = false;
 var playerMoveRight = false;
 
-// Preload game images
+const MOVE_LEFT = 'left';
+const MOVE_RIGHT = 'right';
+
+var LEFT_ARROW_CODE = 37;
+var RIGHT_ARROW_CODE = 39;
+
+
+//  Preload game images
 var images = {};
-['boule-neige.png', 'level.png', 'player.png'].forEach(imgName => {
+[
+    "level.png", "barn.png", "paradise.png",
+
+    "player.png",
+
+    "bouffe-parachute.png",
+
+    "boule-neige.png",
+
+].forEach(imgName => {
     var img = document.createElement('img');
     img.src = 'images/' + imgName;
     images[imgName] = img;
@@ -29,32 +60,11 @@ var images = {};
 
 
 
-
-
 // This section is where you will be doing most of your coding
-class Enemy {
-    constructor(xPos) {
-        this.x = xPos;
-        this.y = -ENEMY_HEIGHT;
-        this.sprite = images['boule-neige.png'];
-
-        // Each enemy should have a different speed
-        this.speed = Math.random() / 2 + 0.25;
-    }
-
-    update(timeDiff) {
-        this.y = this.y + timeDiff * this.speed;
-    }
-
-    render(ctx) {
-        ctx.drawImage(this.sprite, this.x, this.y);
-    }
-}
-
 class Player {
     constructor() {
-        this.x = 2 * PLAYER_WIDTH;
-        this.y = GAME_HEIGHT - PLAYER_HEIGHT - 10;
+        this.x = 4 * PLAYER_WIDTH;
+        this.y = GAME_HEIGHT - PLAYER_HEIGHT - 5;
         this.sprite = images['player.png'];
         this.speed = 0.2;
     }
@@ -76,8 +86,81 @@ class Player {
         ctx.drawImage(this.sprite, this.x, this.y);
     }
 }
+class Enemy {
+    constructor(xPos) {
+        this.x = xPos;
+        this.y = -ENEMY_HEIGHT;
+        this.sprite = images['boule-neige.png'];
+
+        // Each enemy should have a different speed
+        this.speed = Math.random() / 2 + 0.25;
+    }
+
+    update(timeDiff) {
+        this.y = this.y + timeDiff * this.speed;
+    }
+
+    render(ctx) {
+        ctx.drawImage(this.sprite, this.x, this.y);
+    }
+}
+
+class Bouffe {
+    constructor(xPos) {
+        this.x = xPos;
+        this.y = -ENEMY_HEIGHT;
+        this.sprite = images['bouffe-parachute.png'];
+
+        // Each enemy should have a different speed
+        this.speed = 2 + 0.25;
+    }
+
+    update(timeDiff) {
+        this.y = this.y + timeDiff * this.speed;
+    }
+
+    render(ctx) {
+        ctx.drawImage(this.sprite, this.x, this.y);
+    }
+}
 
 
+class Barn {
+    constructor() {
+        this.x = 0;
+        this.y = GAME_HEIGHT - BARN_HEIGHT;
+        this.sprite = images['barn.png'];
+        this.speed = 0.05;
+    }
+
+    // This method is called by the game engine when left/right arrows are pressed
+
+    update(timeDiff) {
+        this.y = this.y + timeDiff * this.speed;
+    }
+
+    render(ctx) {
+        ctx.drawImage(this.sprite, this.x, this.y);
+    }
+}
+class Paradise {
+    constructor() {
+        this.x = 0;
+        this.y = 0 - PARADISE_HEIGHT;
+        this.sprite = images['paradise.png'];
+        this.speed = 0.1;
+    }
+
+    // This method is called by the game engine when left/right arrows are pressed
+
+    update(timeDiff) {
+        this.y = this.y + timeDiff * this.speed;
+    }
+
+    render(ctx) {
+        ctx.drawImage(this.sprite, this.x, this.y);
+    }
+}
 
 
 
@@ -90,6 +173,8 @@ class Engine {
     constructor(element) {
         // Setup the player
         this.player = new Player();
+        this.barn = new Barn();
+        this.paradise = new Paradise();
 
         // Setup enemies, making sure there are always three
         this.setupEnemies();
@@ -133,10 +218,15 @@ class Engine {
         this.enemies[enemySpot] = new Enemy(enemySpot * ENEMY_WIDTH);
     }
 
+    stopScrollingLevel() {
+        document.getElementById("background").style.animationPlayState = "paused"
+    }
+
     // This method kicks off the game
     start() {
         this.score = 0;
         this.lastFrame = Date.now();
+
 
         // Listen for keyboard left/right and update the player
         document.addEventListener('keydown', down => {
@@ -154,6 +244,21 @@ class Engine {
                 playerMoveRight = false;
             }
         })
+        // Space
+        document.addEventListener("keydown", down => {
+            if (down.keyCode === 32) {
+                barnStart = true;
+                document.getElementById("background").classList.remove("background-start")
+                document.getElementById("background").classList.add("background")
+            }
+        })
+        // CTRL
+        document.addEventListener("keydown", down => {
+            if (down.keyCode === 17) {
+                paradiseStart = true;
+            }
+        })
+
 
         this.gameLoop();
     }
@@ -181,7 +286,21 @@ class Engine {
             this.player.update(timeDiff);
         }
 
-        // Increase the score!
+
+        //  Checks if we're at the start or end of the level
+        if (barnStart === true) {
+            this.barn.update(timeDiff);
+        }
+        if (paradiseStart === true) {
+
+            this.paradise.update(timeDiff);
+            console.log(this.paradise.y)
+            if (this.paradise.y >= 0) {
+                didPlayerWinGame = true;
+            }
+        }
+
+        // Score
         this.score += timeDiff;
 
         // Call update on all enemies
@@ -190,15 +309,19 @@ class Engine {
         // Draw everything!
         this.ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
         this.enemies.forEach(enemy => enemy.render(this.ctx)); // draw the enemies
+        this.barn.render(this.ctx); // draw the barn
+        this.paradise.render(this.ctx); // draw the barn
         this.player.render(this.ctx); // draw the player
+
 
         // Check if any enemies should die
         this.enemies.forEach((enemy, enemyIdx) => {
-            if (enemy.y > GAME_HEIGHT) {
+            if (enemy.y > GAME_HEIGHT || paradiseStart) {
                 delete this.enemies[enemyIdx];
             }
         });
         this.setupEnemies();
+
 
         // Check if player is dead
         if (this.isPlayerDead()) {
@@ -206,6 +329,9 @@ class Engine {
             this.ctx.font = 'bold 30px Impact';
             this.ctx.fillStyle = '#ffffff';
             this.ctx.fillText(this.score + ' GAME OVER', 5, 30);
+        }
+        else if (didPlayerWinGame) {
+            this.stopScrollingLevel()
         }
         else {
             // If player is not dead, then draw the score
@@ -221,6 +347,21 @@ class Engine {
 
     isPlayerDead() {
         // TODO: fix this function!
+        for (let i = 0; i < this.enemies.length; i++) {
+            if (this.enemies[i] == undefined) continue;
+            else if (
+                this.enemies[i].x < this.player.x + PLAYER_WIDTH &&
+                this.enemies[i].x + ENEMY_WIDTH > this.player.x &&
+                this.enemies[i].y < this.player.y + PLAYER_HEIGHT &&
+                this.enemies[i].y + ENEMY_HEIGHT > this.player.y) {
+
+                console.log("DEAD")
+                this.stopScrollingLevel()
+                return true;
+            }
+        }
+
+
         return false;
     }
 }
